2d5425b321e015eef6fe0c2a41fc1794b78d8def
sowmya yennam <304sowmya.yennam@gmail.com>
2024-06-02 02:40:52 +0530

    change 2

---
+++ MY_MOMC/Class.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+namespace MY_MOMC
+{
+    internal class Class
+    {
+        internal class User
+        {
+            public int UserId { get; set; }
+            public string UserEmailId { get; set; }
+
+            public List<UserMeetingDetails> usermeetingdetails { get; set; }
+        }
+        internal class UserMeetingDetails
+        {
+            public int MeetingId { get; set;}
+            public string MeetingName { get; set;}
+            public string MeetingStartDateTime { get; set; }
+            public string MeetingEndDateTime { get;set; }
+            public string MeetingDuration { get; set;}
+            public List<MeetingAttendeesEmailName> meetingAttendeesEmailNames { get; set; } 
+            public List<EmailDetails> emails { get; set; }
+        }
+        internal class MeetingAttendeesEmailName
+        {
+            public string AttendeeEmail { get; set; }
+            public string AttendeeName { get; set; }
+        }
+        internal class EmailDetails
+        {
+            public List<string> To { get; set; }
+            public string Subject { get; set; }
+            public string Body { get; set; }
+            public List<string> AttachedFiles { get; set; }
+            public List<string> DrawboardAttaches { get; set; }
+
+            public int EmailSentCount { get; set; }
+        }
+    }
+}
--- MY_MOMC/MainWindow.xaml.cs
+++ MY_MOMC/MainWindow.xaml.cs
@@ -1,31 +1,93 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
+using System.Net.Mail;
+using System.Security.Cryptography;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace MY_MOMC
 {
     public partial class MainWindow : Window
     {
+        internal List<MY_MOMC.Class.User> users;
+        internal static string email;
         public MainWindow()
         {
             InitializeComponent();
+            OnLoad();
         }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
+        internal void OnLoad()
         {
-            VerificationPage myUserControl = new VerificationPage();
-            this.Content = myUserControl;
+            string jsonFilePath = "C:\\MY_PROJECT\\MY_MOMC\\MY_MOMC\\data.json";
+            string readJsonFile=File.ReadAllText(jsonFilePath);
+            users = JsonConvert.DeserializeObject<List<MY_MOMC.Class.User>>(readJsonFile);
+        }
+        private void Continue_Click(object sender, RoutedEventArgs e)
+        {
+            email = tb_email.Text.Trim();
+            try
+            {
+                if (tb_email.Text.Trim().Length > 0)
+                {
+                    if (users.Any(user => user.UserEmailId == email))
+                    {
+                        int randomDigits = GenerateRandomNumber(100000, 999999);
+                        var result = MessageBox.Show("OTP sent successfully! Are you sure you want to continue with this email", "Information", MessageBoxButton.OKCancel, MessageBoxImage.Information);
+                        if (result == MessageBoxResult.OK)
+                        {
+                            MailMessage mail = new MailMessage();
+                            mail.From = new MailAddress(email);
+                            mail.To.Add(tb_email.Text.Trim());
+                            mail.Subject = "Your OTP";
+                            mail.Body = $"Otp is {randomDigits}";
+
+                            SmtpClient smtpClient = new SmtpClient("smtp.gmail.com");
+                            smtpClient.Port = 587;
+                            smtpClient.Credentials = new NetworkCredential(email, "tkki grgd aapo uavx\r\n");
+                            smtpClient.EnableSsl = true;
+                            smtpClient.Send(mail);
+
+                            MessageBox.Show("Email sent successfully.");
+                            VerificationPage vp = new VerificationPage();
+                            this.Content = vp;
+                        }
+                        else if (result == MessageBoxResult.Cancel)
+                        {
+                            tb_email.Text = "";
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Invalid Email");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Enter Email");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+            }
+        }
+        private int GenerateRandomNumber(int min, int max)
+        {
+            using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+            {
+                byte[] randomNumber = new byte[4];
+                Random random = new Random();
+                random.NextBytes(randomNumber);
+                int value = BitConverter.ToInt32(randomNumber, 0);
+                value = Math.Abs(value);
+                int randomRange = max - min + 1;
+                int randomOffset = value % randomRange;
+                int randomResult = randomOffset + min;
+                return randomResult;
+            }
         }
     }
 }
